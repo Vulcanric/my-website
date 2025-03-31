513e3616c29f8680c23a4122a544b407a251aea0
Vulcanric <jneric49@gmail.com>
2025-04-01 00:59:51 +0100

    Attached first section reference link to logo

--- Client/src/components/Logo.jsx
+++ Client/src/components/Logo.jsx
@@ -3,13 +3,15 @@ import me from "../assets/images/me.jpg";
 const Logo = ( {className, size} ) => {
 
   return (
-    <div className={className}>
-      <img
-        src={me}
-        width={size}
-        alt="My logo"
-      />
-    </div>
+    <a href="#home" >
+      <div className={className}>
+        <img
+          src={me}
+          width={size}
+          alt="My logo"
+          />
+      </div>
+    </a>
   )
 }
 
