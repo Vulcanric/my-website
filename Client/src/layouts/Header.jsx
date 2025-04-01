d5d5ee56fa9d5d678cfb2c6e7730e7324a08a987
Vulcanric <jneric49@gmail.com>
2025-04-01 23:19:42 +0100

    Allow NavBar to set child 'className' property

--- Client/src/layouts/Header.jsx
+++ Client/src/layouts/Header.jsx
@@ -20,14 +20,20 @@ const Header = () => {
 
   return (
     <div className="w-full h-20 flex flex-col items-center border justify-center fixed" >
-      <div className="w-full h-full flex items-center justify-between border">
-        <Logo size="50" className="m-5" />
-        <NavBar className="sm:hidden md:flex lg:flex justify-center items-center gap-12" />
+      <div className="w-full h-full flex items-center sm:justify-between md:justify-start border bg-white">
+        <Logo size="50" className="m-5" to="#home" description="John Eric" />
+        <NavBar
+          className="sm:hidden md:flex lg:flex justify-center items-center gap-12 mx-auto"
+          childClass=""
+        />
         <MenuButton onClick={() => setContextMenu(!contextMenu)} className="w-8 h-6 mx-6 flex flex-col cursor-pointer justify-between md:hidden lg:hidden z-10" />
       </div>
       {
         contextMenu && (
-          <NavBar className="bg-white flex flex-col border w-full h-screen top-0 items-center gap-8 py-12 absolute" />
+          <NavBar
+            className="bg-white flex flex-col border w-full h-screen top-0 items-center gap-9 py-12 absolute"
+            childClass="text-xl"
+          />
         )
       }
     </div>
