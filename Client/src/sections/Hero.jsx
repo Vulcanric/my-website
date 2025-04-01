17ee2faecd88020b845d208a885084671b09f0b6
Vulcanric <jneric49@gmail.com>
2025-04-01 01:01:07 +0100

    Made it first thing a visitor sees

--- Client/src/sections/Hero.jsx
+++ Client/src/sections/Hero.jsx
@@ -3,9 +3,10 @@
 const Hero = () => {
 
     return (
-        <section className="h-full w-full border flex flex-col items-center justify-center">
+        <section id="home" className="h-full w-full border flex flex-col items-center justify-center">
             <p className="text-xl text-center">Hi 👋, I'm</p>
             <h1 className="text-6xl text-center">John Eric</h1>
+            
         </section>
     )
 }
