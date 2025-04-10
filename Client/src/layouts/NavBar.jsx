e8183a27aef34d0a36b3a0588bded16974b8d780
Vulcanric <jneric49@gmail.com>
2025-04-10 16:27:20 +0100

    Personalized nav link labels

--- Client/src/layouts/NavBar.jsx
+++ Client/src/layouts/NavBar.jsx
@@ -3,11 +3,11 @@ import NavLink from "../components/NavLink"
 const NavBar = ( {className, childClass} ) => {
     return (
       <div className={className}>
-        <NavLink label="About Me" to="#about" className={childClass} />
+        <NavLink label="About me" to="#about" className={childClass} />
         <NavLink label="Services" to="#services" className={childClass} />
-        <NavLink label="Work" to="#work" className={childClass} />
+        <NavLink label="My work" to="#work" className={childClass} />
         <NavLink label="Testimonials" to="#testimonials" className={childClass} />
-        <NavLink label="Contact" to="#contact" className={childClass} />
+        <NavLink label="Contact me" to="#contact" className={childClass} />
       </div>
     )
 }
