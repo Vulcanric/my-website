88eb23332105429e5e2729c649984926c1cce82a
Vulcanric <jneric49@gmail.com>
2025-04-15 00:54:27 +0100

    Updated hero section with titles

--- Client/src/sections/Hero.jsx
+++ Client/src/sections/Hero.jsx
@@ -3,9 +3,11 @@
 const Hero = () => {
 
     return (
-        <section id="home" className="h-full w-full border flex flex-col items-center justify-center">
+        <section id="home" className="h-full w-full border flex flex-col items-center justify-center gap-4">
             <p className="text-xl text-center">Hi 👋, I'm</p>
-            <h1 className="text-6xl text-center">John Eric</h1>
+            <h1 className="text-6xl font-bold text-center">Eric John</h1>
+            <code className="text-2xl font-bold text-blue-500">{"{ Software Engineer }"}</code>
+            <code className="text-2xl font-bold text-blue-500">{"{ DevOps Engr }"}</code>
         </section>
     )
 }
