b7f621937012541c55c787a01a9c98e3a5057861
Vulcanric <jneric49@gmail.com>
2025-04-15 00:42:31 +0100

    Included skill section

--- Client/src/layouts/Body.jsx
+++ Client/src/layouts/Body.jsx
@@ -1,6 +1,7 @@
 import Hero from "../sections/Hero";
 import AboutMe from "../sections/AboutMe";
 import Services from "../sections/Services";
+import Skills from "../sections/Skills";
 import Work from "../sections/Work";
 import Testimonials from "../sections/Testimonials";
 import Contact from "../sections/Contact";
@@ -8,10 +9,11 @@ import Contact from "../sections/Contact";
 const Body = () => {
 
     return (
-        <div className="h-screen" style={{scrollSnapType: "mandatory"}}>
+        <div id="content" className="h-screen" style={{scrollSnapType: "mandatory"}}>
             <Hero />
             <AboutMe />
             <Services />
+            <Skills />
             <Work />
             <Testimonials />
             <Contact />
