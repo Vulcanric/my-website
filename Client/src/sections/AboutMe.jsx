c4a994ca8f67d1d98b6b939d91ed9befe9cd17f1
Vulcanric <jneric49@gmail.com>
2025-04-15 00:52:26 +0100

    Updated

--- Client/src/sections/AboutMe.jsx
+++ Client/src/sections/AboutMe.jsx
@@ -4,13 +4,22 @@ import Logo from "../components/Logo"
 const AboutMe = () => {
 
   return (
-    <section id="about" className="w-full p-6 border flex flex-col items-center justify-center overflow-y-visible bg-black" style={{scrollSnapAlign: "start"}}>
-      <Logo className="rounded-4xl" size={200} />
-      <p className="p-5 text-white">Hello, I'm Eric! I'm a full-stack software developer with experience in Python, TypeScript, SQL, and noSQL. I've built and maintained web applications from the ground up, focusing on clean, effective, and scalable code. My journey started at a young age, driven by a love for problem-solving and creating innovative solutions. Recently, I've been working on a project that leverages OCR and image-based product detection, applying modern technologies to real-world challenges. I'm commited to continuous learning and excited about any opportunity to collaborate and make a difference through tech.</p>
-      <hr />
+    <section
+      id="about"
+      className="w-full p-8 border flex flex-col items-center overflow-y-visible"
+      style={{scrollSnapAlign: "start"}}
+    >
+      <h1 className="text-4xl font-bold mt-5 mb-11">About me</h1>
+      <Logo
+        size={200}
+        className="rounded-4xl"
+      />
+      <p className="p-5 text-lg">Hello, I'm Eric! I'm a full-stack software developer with experience in Python, TypeScript, SQL, and noSQL. I've built and maintained web applications from the ground up, focusing on clean, effective, and scalable code. My journey started at a young age, driven by a love for problem-solving and creating innovative solutions. Recently, I've been working on a project that leverages OCR and image-based product detection, applying modern technologies to real-world challenges. I'm commited to continuous learning and excited about any opportunity to collaborate and make a difference through tech.</p>
+      <br />
       <Card
-        className="flex sm:flex-col p-5 border w-96 h-80 bg-gray-50"
+        className="flex sm:flex-col my-10 border w-96 h-80 bg-gray-50 border align-center justify-center"
       >
+        <button className="text-4xl border">+</button>
       </Card>
     </section>
   )
