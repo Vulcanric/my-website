57d9c041a6caebc3cde36b2429dd0a204dbbe1ae
Vulcanric <jneric49@gmail.com>
2025-04-16 23:57:27 +0100

    Fix: changed attribute 'class' to 'className'

--- Client/src/layouts/AppScreen.jsx
+++ Client/src/layouts/AppScreen.jsx
@@ -17,7 +17,7 @@ const AppScreen = ({ picture, linkTo, width, height }) => {
         viewBox="0 0 24 24" fill="none"
         stroke="#ffffff" stroke-width="2"
         stroke-linecap="round" stroke-linejoin="round"
-        class="absolute top-5 right-6 group-hover:block hidden cursor-pointer"
+        className="absolute top-5 right-6 group-hover:block hidden cursor-pointer"
       >
         <path d="M15 3h6v6"></path><path d="M10 14 21 3"></path><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
       </svg>
