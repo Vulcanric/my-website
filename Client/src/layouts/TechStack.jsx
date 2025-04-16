a4744481be0de6dcc881e5ae50149400f219da80
Vulcanric <jneric49@gmail.com>
2025-04-16 01:20:18 +0100

    Update: Added skills 'Bash', 'Linux', and 'Markdown'. Icon Expressjs could not be found

--- Client/src/layouts/TechStack.jsx
+++ Client/src/layouts/TechStack.jsx
@@ -6,13 +6,16 @@ const TechStack = () => {
     {name: "Python", iconName: "python", wiki: "https://en.wikipedia.org/wiki/Python_(programming_language)"},
     {name: "C / C++", iconName: "c++", wiki: "https://en.wikipedia.org/wiki/C_(programming_language)"},
     {name: "TypeScript", iconName: "typescript", wiki: "https://en.wikipedia.org/wiki/TypeScript"},
+    {name: "Bash", iconName: "bash", wiki: "https://en.wikipedia.org/wiki/Bash_(Unix_shell)"},
     {name: "HTML5", iconName: "html5", wiki: "https://en.wikipedia.org/wiki/HTML5"},
     {name: "CSS3", iconName: "css3", wiki: "https://en.wikipedia.org/wiki/CSS"},
     {name: "JavaScript", iconName: "js", wiki: "https://simple.wikipedia.org/wiki/JavaScript"},
     {name: "React.js", iconName: "reactjs", wiki: "https://en.wikipedia.org/wiki/React_(software)"},
     {name: "Tailwind CSS", iconName: "tailwindcss", wiki: "https://simple.wikipedia.org/wiki/Tailwind_CSS"},
+    // {name: "Express.js", iconName: "expressjs", wiki: "https://en.wikipedia.org/wiki/Express.js"},
     {name: "Django", iconName: "django", wiki: "https://en.wikipedia.org/wiki/Django_(web_framework)"},
     {name: "Flask", iconName: "flask", wiki: "https://en.wikipedia.org/wiki/Flask_(web_framework)"},
+    {name: "Markdown", iconName: "markdown", wiki: "https://en.wikipedia.org/wiki/Markdown"},
     {name: "MySQL", iconName: "mysql", wiki: "https://en.wikipedia.org/wiki/MySQL"},
     {name: "Redis", iconName: "redis", wiki: "https://en.wikipedia.org/wiki/Redis"},
     {name: "MongoDB", iconName: "mongodb", wiki: "https://en.wikipedia.org/wiki/MongoDB"},
@@ -21,7 +24,8 @@ const TechStack = () => {
     {name: "Git", iconName: "git", wiki: "https://en.wikipedia.org/wiki/Git"},
     {name: "Miro", iconName: "miro", wiki: "https://en.wikipedia.org/wiki/Miro_(collaboration_platform)"},
     {name: "Figma", iconName: "figma", wiki: "https://en.wikipedia.org/wiki/Figma"},
-    {name: "GPT", iconName: "openai", wiki: "https://community.openai.com/t/based-on-specific-wikipedia-page/34507"}
+    {name: "OpenAI", iconName: "openai", wiki: "https://community.openai.com/t/based-on-specific-wikipedia-page/34507"},
+    {name: "Linux", iconName: "linux", wiki: "https://en.wikipedia.org/wiki/Linux"}
   ]
 
   return (
