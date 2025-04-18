f281e952a31d5bba5ca4abaf2e5f54233065b7dd
Vulcanric <jneric49@gmail.com>
2025-04-18 23:04:58 +0100

    Updated about me

--- Client/src/sections/AboutMe.jsx
+++ Client/src/sections/AboutMe.jsx
@@ -6,15 +6,17 @@ const AboutMe = () => {
   return (
     <section
       id="about"
-      className="w-full p-8 border flex flex-col items-center overflow-y-visible"
+      className="w-full p-8 border flex flex-col items-center overflow-y-visible bg-black"
       style={{scrollSnapAlign: "start"}}
     >
-      <h1 className="text-4xl font-bold mt-5 mb-11">About me</h1>
+      <h1 className="text-4xl text-white font-bold mt-5 mb-11">About me</h1>
       <Logo
         size={200}
-        className="rounded-4xl"
+        className="rounded-4xl border rounded-full p-2 bg-white"
       />
-      <p className="p-5 text-lg">Hello, I'm Eric! I'm a full-stack software developer with experience in Python, TypeScript, SQL, and noSQL. I've built and maintained web applications from the ground up, focusing on clean, effective, and scalable code. My journey started at a young age, driven by a love for problem-solving and creating innovative solutions. Recently, I've been working on a project that leverages OCR and image-based product detection, applying modern technologies to real-world challenges. I'm commited to continuous learning and excited about any opportunity to collaborate and make a difference through tech.</p>
+      <br />
+      <p className="px-10 py-5 text-lg text-white">Hello, I'm Eric! I'm a full-stack software developer with experience in Python, TypeScript, SQL, and noSQL. I've built and maintained web applications from the ground up, focusing on clean, effective, and scalable code.</p>
+      <p className="px-10 py-5 text-lg text-white">My journey started at a young age, driven by a love for problem-solving and creating innovative solutions. Recently, I've been working on a project that leverages OCR and image-based product detection, applying modern technologies to real-world challenges. I'm commited to continuous learning and excited about any opportunity to collaborate and make a difference through tech.</p>
       <br />
       <Card
         className="flex sm:flex-col my-10 border w-96 h-80 bg-gray-50 border align-center justify-center"
