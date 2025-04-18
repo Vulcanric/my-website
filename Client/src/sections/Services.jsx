ad3dae42645b7bb674d9c997ffacc0f4c72491d1
Vulcanric <jneric49@gmail.com>
2025-04-18 23:07:16 +0100

    Incomplete: my services card

--- Client/src/sections/Services.jsx
+++ Client/src/sections/Services.jsx
@@ -1,10 +1,29 @@
-
+import Card from "../components/Card"
 
 const Services = () => {
 
-    return (
-        <section id="services" className="h-full border">
-        </section>
+  return (
+    <section id="services" className="border flex flex-col items-center p-8">
+      <h1 className="text-4xl mt-5 mb-11">Services</h1>
+      <div className="flex flex-col md:flex-row flex-wrap justify-evenly gap-10">
+        <Card
+          className="border w-96 h-96 border-2 border-gray-300 rounded-lg"
+        >
+        </Card>
+        <Card
+          className="border w-96 h-96 border-2 border-gray-300 rounded-lg"
+        >
+        </Card>
+        <Card
+          className="border w-96 h-96 border-2 border-gray-300 rounded-lg"
+        >
+        </Card>
+        <Card
+          className="border w-96 h-96 border-2 border-gray-300 rounded-lg"
+        >
+        </Card>
+      </div>
+    </section>
     )
 }
-export default Services
+export default Services;
