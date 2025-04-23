28d1fc5e7e5fbbbb03ef8ce5e8f2352ed4cdd2b0
Vulcanric <jneric49@gmail.com>
2025-04-23 02:07:25 +0100

    Improved work section UI

--- Client/src/sections/Work.jsx
+++ Client/src/sections/Work.jsx
@@ -1,5 +1,6 @@
 import Card from "../components/Card"
 import AppScreen from "../layouts/AppScreen";
+import TechChip from "../components/TechChip";
 
 import REMOTELY from "../assets/images/remotely.png";
 import LILY from "../assets/images/lily.png";
@@ -10,7 +11,7 @@ import StackIcon from "tech-stack-icons";
 
 const Work = () => {
 
-  const experience = [
+  const experiences = [
     {
       title: "Remotely",
       picture: REMOTELY,
@@ -58,36 +59,45 @@ const Work = () => {
       <br />
       <div className="flex flex-wrap items-center justify-center gap-10">
         {
-          experience.map((work, index) => {
+          experiences.map((work, index) => {
             return (
+              <>
               <Card key={index} className="bg-white flex flex-col
-              items-center justify-center p-5 px-10 border-2 rounded-2xl
+              items-center justify-center py-5 px-10 rounded-2xl
               "
               >
-                <div className="flex gap-36 items-center w-full">
-                  <div className="sm:hidden md:block bottom-0">
+                <div
+                  className={
+                    `flex ${index % 2 === 0 ? "flex-row" : "flex-row-reverse"}
+                    gap-20 justify-center w-full p-5`
+                  }
+                >
+                  <div className="sm:hidden md:block bottom-0 w-1/2">
                     <h2 className="text-3xl font-bold">{work.title}</h2>
                     <br />
-                    <div className="flex gap-10">
-                      <StackIcon name="reactjs" className="w-10"/>
-                      <StackIcon name="nestjs" className="w-10"/>
-                      <StackIcon name="tailwindcss" className="w-10" />
-                      <StackIcon name="redis" className="w-10" />
+                    <p className="text-pretty text-wrap text-lg">{work.description}</p>
+                    <div>
+                      {/* Include Tech chips here */}
                     </div>
                   </div>
-                  <AppScreen picture={work.picture} linkTo={work.web.url} width={400} height={200} />
+                  <AppScreen
+                    picture={work.picture}
+                    linkTo={work.web.url}
+                    alt={work.title}
+                    width={450} height={250}
+                  />
                 </div>
                 <br />
-                <div className="">
-                  <h2 className="sm:block md:hidden text-3xl font-bold">{work.title}<br /></h2>
-                  <p className="text-pretty text-wrap border text-lg">{work.description}</p>
-                  <br />
-                  <div className="flex justify-between">
-                    <button className="border px-8 py-2 bg-blue-200 rounded-xl">GitHub</button>
-                    <button className="border px-8 py-2 bg-blue-200 rounded-xl">Live view</button>
-                  </div>
+                <div className="self-end border w-96 flex justify-between">
+                  <button className="border px-8 py-2 bg-blue-200 rounded-xl">GitHub</button>
+                  <button className="border px-8 py-2 bg-blue-200 rounded-xl">Live view</button>
                 </div>
               </Card>
+              {//Include a horizontal after each work experience
+                index != experiences.length - 1 // Don't include for last experience
+                && <hr className="w-full" />
+              }
+              </>
             );
           })
         }
