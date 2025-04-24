3b6fd36f65672fb4e3a0694d3a1474dca082cdfa
Vulcanric <jneric49@gmail.com>
2025-04-24 23:46:28 +0100

    Updated

--- Client/src/sections/Skills.jsx
+++ Client/src/sections/Skills.jsx
@@ -4,8 +4,9 @@ import TechStack from "../layouts/TechStack";
 const Skills = () => {
 
     return (
-        <section id="contact" className="flex flex-col items-center p-8 border">
+        <section id="skills" className="flex flex-col items-center p-8 border">
             <h2 className="text-4xl font-bold mt-5 mb-11">My Skills</h2>
+            <br />
             <br />
             <TechStack />
             <br />
