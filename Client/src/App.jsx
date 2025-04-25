d5246d0becf22174d7e48e31b8b7d1e40ea236ff
Vulcanric <jneric49@gmail.com>
2025-04-25 05:41:22 +0100

    App structure Component

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./layouts/Header";
+import Body from './layouts/Body';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Body />
+    </>
+  );
+}
+
+export default App;
