08e2bf111032939519c96ee8725acdbf91672c5d
Vulcanric <jneric49@gmail.com>
2025-04-28 00:52:05 +0100

    Update: Improved Hero section design, added links to social media platforms

--- Client/src/sections/Hero.jsx
+++ Client/src/sections/Hero.jsx
@@ -1,14 +1,39 @@
-
+import BACKGROUND from '../assets/images/3D.png';
 
 const Hero = () => {
 
-    return (
-        <section id="home" className="h-full w-full border flex flex-col items-center justify-center gap-4">
-            <p className="text-xl text-center">Hi 👋, I'm</p>
-            <h1 className="text-6xl font-bold text-center">Eric John</h1>
-            <code className="text-2xl font-bold text-blue-500">{"{ Software Engineer }"}</code>
-            <code className="text-2xl font-bold text-blue-500">{"{ DevOps Engr }"}</code>
-        </section>
-    )
+  return (
+    <section id="home" style={{
+        backgroundImage: `url(${BACKGROUND})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+      }}
+      className="h-full w-full flex flex-col items-center justify-end gap-4 relative"
+    >
+      <h1 className="text-9xl font-bold text-center text-slate-300 drop-shadow-xl">ERIC JOHN</h1>
+      <p className="text-2xl text-center text-slate-200 font-bold backdrop-blur-xl">SOFTWARE ENGINEER, BACKEND & APP DEVELOPER</p>
+      <div
+        className='border-2 border-white w-7 h-12 rounded-full hover:cursor-pointer flex flex-col items-center py-1 absolute bottom-5'
+        onClick={() => {document.location.href = '#about'}}
+      >
+        <div 
+          className='w-2 h-4 bg-white rounded-full animate-scroll'
+        />
+      </div>
+      <br />
+      <div className='flex gap-10 mb-36'>
+        <a href="https://x.com/JohnEri89510617">
+          <i className="fa-brands fa-x-twitter fa-xl text-white" />
+        </a>
+        <a href="http://github.com/vulcanric">
+          <i className="fa-brands fa-github fa-xl text-white" />
+        </a>
+        <a href="http://www.linkedin.com/in/johneric1">
+          <i className="fa-brands fa-linkedin fa-xl text-white" />
+        </a>
+      </div>
+      <br />
+    </section>
+  )
 }
-export default Hero
+export default Hero;
