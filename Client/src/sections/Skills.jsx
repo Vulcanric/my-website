c93eb7aa1ed674d915a322acc631b564bef9e13e
Vulcanric <jneric49@gmail.com>
2025-04-28 00:52:34 +0100

    Unfinished

--- Client/src/sections/Skills.jsx
+++ Client/src/sections/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = () => {
 
     return (
         <section id="skills" className="flex flex-col items-center p-8 border">
-            <h2 className="text-4xl font-bold mt-5 mb-11">My Skills</h2>
+            <h2 className="text-5xl mt-5 mb-11">My skills</h2>
             <br />
             <br />
             <TechStack />
