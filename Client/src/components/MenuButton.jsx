10f52d5783d6b8df0de427fb4c1d2a51118259cc
Vulcanric <jneric49@gmail.com>
2025-04-28 00:44:00 +0100

    Animated menu button and fixed indefinite height with css min-height

--- Client/src/components/MenuButton.jsx
+++ Client/src/components/MenuButton.jsx
@@ -1,12 +1,12 @@
-const MenuButton = ( { onClick, className } ) => {
+const MenuButton = ( { onClick, className, isOpen } ) => {
   return (
     <button
       className={className}
       onClick={onClick}
     >
-      <span className="bg-gray-400 h-1 rounded"></span>
-      <span className="bg-gray-400 h-1 rounded"></span>
-      <span className="bg-gray-400 h-1 rounded"></span>
+      <span className={`w-9 transition-[transform] duration-300 ease-linear ${isOpen && "rotate-45 translate-y-2"} bg-gray-400 min-h-1 rounded`}></span>
+      <span className={`w-9 transition-[transform] duration-300 ease-linear ${isOpen && "-rotate-[45deg] -translate-y-1"} bg-gray-400 min-h-1 rounded`}></span>
+      <span className={`${isOpen && "hidden"} w-9 bg-gray-400 min-h-1 rounded`}></span>
     </button>
   )
 }
