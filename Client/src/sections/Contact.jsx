3f7b3f7e0f8babb7117fffd4eaf6155e8ece07da
Vulcanric <jneric49@gmail.com>
2025-04-28 00:49:21 +0100

    Updated Contact styling

--- Client/src/sections/Contact.jsx
+++ Client/src/sections/Contact.jsx
@@ -2,12 +2,12 @@
 const Contact = () => {
 
   return (
-    <section id="contact" className="h-full border flex flex-col items-center">
+    <section id="contact" className="min-h-full border flex flex-col items-center">
       <h2 className="text-5xl my-14">Contact me</h2>
       <form
         action="/contact-form"
         method="post"
-        className="form-container flex flex-col gap-5 bg-gray-50 border p-10 rounded-2xl"
+        className="form-container flex flex-col gap-5 border p-10 rounded-2xl shadow-xl"
       >
         <div className="w-full flex flex-col">
           <label className="my-2" htmlFor="name">
@@ -18,7 +18,7 @@ const Contact = () => {
             name="name"
             id="name"
             placeholder="John Doe"
-            className="bg-white h-12 p-2 border rounded-lg w-98"
+            className="bg-white h-12 py-2 px-4 border-2 focus:outline-offset-2 focus:outline-red-100 rounded-xl w-98"
           />
           <br />
           <label className="my-2" htmlFor="email">
@@ -29,10 +29,10 @@ const Contact = () => {
             name="email"
             id="email"
             placeholder="johndoe@mail.com"
-            className="bg-white h-12 p-2 border focus:outline-offset-2 focus:outline-orange-300 rounded-lg"
+            className="bg-white h-12 py-2 px-4 border-2 focus:outline-offset-2 focus:outline-red-100 rounded-xl"
           />
           <br />
-          <label className="my-2" htmlFor="message">
+          <label className="my-2 text-lg" htmlFor="message">
             Message
           </label>
           <textarea
@@ -40,13 +40,13 @@ const Contact = () => {
             id="message"
             placeholder="Let's work together!"
             rows={3}
-            className="bg-white p-2 border rounded-lg"
+            className="bg-white py-2 px-4 border-2 focus:outline-offset-2 focus:outline-red-100 rounded-xl"
           >
           </textarea>
         </div>
         <div className="w-full border">
           <button
-            className="right-0 px-6 py-2 rounded-xl text-xl bg-yellow-300 hover:bg-yellow-400"
+            className="right-0 px-6 py-2 rounded-xl text-lg w-full bg-orange-400 hover:bg-yellow-400"
           >
             Submit
           </button>
