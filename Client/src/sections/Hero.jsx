b05620d69d2cf1f35a83838efb4ab68e4147cc0b
Vulcanric <jneric49@gmail.com>
2025-04-29 00:39:51 +0100

    Included a dark grey background + made section height screen height

--- Client/src/sections/Hero.jsx
+++ Client/src/sections/Hero.jsx
@@ -8,10 +8,10 @@ const Hero = () => {
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover'
       }}
-      className="h-full w-full flex flex-col items-center justify-end gap-4 relative"
+      className="h-screen w-full bg-[#333333] flex flex-col items-center justify-end gap-4 relative"
     >
       <h1 className="text-9xl font-bold text-center text-slate-300 drop-shadow-xl">ERIC JOHN</h1>
-      <p className="text-2xl text-center text-slate-200 font-bold backdrop-blur-xl">SOFTWARE ENGINEER, BACKEND & APP DEVELOPER</p>
+      <p className="text-2xl text-center text-slate-200 font-bold backdrop-blur-xl">SOFTWARE ENGINEER, BACKEND & WEB DEVELOPER</p>
       <div
         className='border-2 border-white w-7 h-12 rounded-full hover:cursor-pointer flex flex-col items-center py-1 absolute bottom-5'
         onClick={() => {document.location.href = '#about'}}
