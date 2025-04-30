7873a11ec773952796c669ecafbf95893595494a
Vulcanric <jneric49@gmail.com>
2025-04-30 23:58:40 +0100

    Unfinished

--- Client/src/sections/Services.jsx
+++ Client/src/sections/Services.jsx
@@ -3,7 +3,7 @@ import Card from "../components/Card"
 const Services = () => {
 
   return (
-    <section id="services" className="border flex flex-col items-center p-8">
+    <section id="services" className="bg-[#444444] flex flex-col items-center p-8">
       <h1 className="text-4xl mt-5 mb-11">Services</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
         <Card
