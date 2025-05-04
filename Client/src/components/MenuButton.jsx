14c1da5d4e6bdb716e081f8e830e0df5e1fcbd02
Vulcanric <jneric49@gmail.com>
2025-05-04 23:52:38 +0100

    Update: reduce bar size, animate 3rd bar

--- Client/src/components/MenuButton.jsx
+++ Client/src/components/MenuButton.jsx
@@ -4,9 +4,9 @@ const MenuButton = ( { onClick, className, isOpen } ) => {
       className={className}
       onClick={onClick}
     >
-      <span className={`w-9 transition-[transform] duration-300 ease-linear ${isOpen && "rotate-45 translate-y-2"} bg-gray-400 min-h-1 rounded`}></span>
-      <span className={`w-9 transition-[transform] duration-300 ease-linear ${isOpen && "-rotate-[45deg] -translate-y-1"} bg-gray-400 min-h-1 rounded`}></span>
-      <span className={`${isOpen && "hidden"} w-9 bg-gray-400 min-h-1 rounded`}></span>
+      <span className={`w-8 transition-[transform] duration-300 ease-linear ${isOpen && "rotate-45 translate-y-2"} bg-gray-400 min-h-1 rounded`}></span>
+      <span className={`w-8 transition-[transform] duration-300 ease-linear ${isOpen && "-rotate-[45deg] -translate-y-1"} bg-gray-400 min-h-1 rounded`}></span>
+      <span className={`w-8 transition-[transform] duration-300 ease-linear ${isOpen ? "scale-x-0" : "scale-x-100"} bg-gray-400 min-h-1 rounded`}></span>
     </button>
   )
 }
