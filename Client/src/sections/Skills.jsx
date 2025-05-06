c1ed90992b451c6d6efea7507d97ae646891646f
Vulcanric <jneric49@gmail.com>
2025-05-06 10:09:56 +0100

    Update: removed ugly navy blue background colour

--- Client/src/sections/Skills.jsx
+++ Client/src/sections/Skills.jsx
@@ -4,7 +4,7 @@ import TechStack from "../layouts/TechStack";
 const Skills = () => {
 
     return (
-        <section id="skills" className="bg-[#1A1F36] flex flex-col items-center p-8 border">
+        <section id="skills" className=" flex flex-col items-center p-8 border">
             <h2 className="text-5xl mt-5 mb-11">My skills</h2>
             <br />
             <br />
