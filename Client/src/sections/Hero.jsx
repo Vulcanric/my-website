28beae8b695520ae762115ce473160cb4c2b5714
Vulcanric <jneric49@gmail.com>
2025-06-03 22:55:02 +0100

    Update Hero section styles and content for improved layout and clarity

--- Client/src/sections/Hero.jsx
+++ Client/src/sections/Hero.jsx
@@ -3,12 +3,32 @@ const Hero = () => {
 
   return (
     <section id="home"
-      className="h-screen w-full bg-left-top bg-[url('/3D-mobile1.png')] md:bg-[url('/3D-desktop.png')] bg-no-repeat bg-cover flex flex-col items-center justify-end gap-4 relative"
+      className="h-screen w-full bg-[#0c1217] bg-right-top bg-[url('/3D-mobile1.png')] md:bg-[url('/3D-desktop.png')] bg-no-repeat bg-cover flex flex-col items-center justify-end gap-4 relative"
+      style={{
+        // backgroundPosition: 'center',
+        // backgroundSize: 'cover',
+        backgroundAttachment: 'fixed',
+      }}
     >
-      <h1 className="text-9xl font-bold text-center text-slate-200 drop-shadow-xl">ERIC JOHN</h1>
-      <p className="text-2xl text-center text-slate-200 font-bold backdrop-blur-xl">SOFTWARE ENGINEER, BACKEND & WEB DEVELOPER</p>
+      <div className="size-fit absolute top-1/4 md:top-1/3 flex flex-col items-center">
+        <h1 className="text-8xl md:text-9xl font-bold text-center text-slate-200 drop-shadow-xl">ERIC JOHN</h1>
+        <br />
+        <p className="w-fit px-5 text-2xl text-center text-slate-200 font-bold backdrop-blur-xl">SOFTWARE ENGINEER, FULLSTACK & WEB DEVELOPER</p>
+        <br />
+        <div className='flex size-fit gap-10 mb-36'>
+          <a href="https://x.com/JohnEri89510617">
+            <i className="fa-brands fa-x-twitter fa-xl text-white" />
+          </a>
+          <a href="http://github.com/vulcanric">
+            <i className="fa-brands fa-github fa-xl text-white" />
+          </a>
+          <a href="http://www.linkedin.com/in/johneric1">
+            <i className="fa-brands fa-linkedin fa-xl text-white" />
+          </a>
+        </div>
+      </div>
       <div
-        className='border-2 border-white w-7 h-12 rounded-full hover:cursor-pointer flex flex-col items-center py-1 absolute bottom-8'
+        className='border-2 border-white w-7 h-12 rounded-full hover:cursor-pointer flex flex-col items-center py-1 absolute bottom-[3rem]'
         onClick={() => {document.location.href = '#about'}}
       >
         <div 
@@ -26,18 +46,6 @@ const Hero = () => {
 	        <span>⭐</span>STAR
         </div>
       </a>
-      <br />
-      <div className='flex gap-10 mb-36'>
-        <a href="https://x.com/JohnEri89510617">
-          <i className="fa-brands fa-x-twitter fa-xl text-white" />
-        </a>
-        <a href="http://github.com/vulcanric">
-          <i className="fa-brands fa-github fa-xl text-white" />
-        </a>
-        <a href="http://www.linkedin.com/in/johneric1">
-          <i className="fa-brands fa-linkedin fa-xl text-white" />
-        </a>
-      </div>
       <br />
     </section>
   )
